6ec2f4d5965e4a40d2534d0962ce05a56eb34a24
Mister-maker <mkaggrawal97@gmail.com>
2019-03-31 19:58:37 +0530

    animation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import './moduls/test';
 import './views/testView';
+import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
+
+
+TweenMax.to(('#cartoon'), 1, {scale: 1});
 
 console.log("Hello World 2");

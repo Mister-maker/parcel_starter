ba9defe266c15cf581ebaba06678d28b2460c034
Mister-maker <mkaggrawal97@gmail.com>
2021-01-15 13:01:00 +0530

    files added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import './moduls/test';
 import './views/testView';
-import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
+// import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
 
 
 TweenMax.to(('#cartoon'), 1, {scale: 1});
